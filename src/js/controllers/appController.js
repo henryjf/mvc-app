42cde20da4a3569b038ddd9fc70ac5e4cfa60f69
JEFF HENRY <henry_jf@hotmail.com>
2016-05-16 20:53:24 -0400

    working on template to get data to page

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -25,20 +25,19 @@ formSubmit() {
 
     // console.log(this);
 
-    let firstName = form.find('#firstNameInput').val();
-    let lastName  = form.find('#lastNameInput').val();
-    let location  = form.find('#locationInput').val();
-    let phone     = form.find('#phoneInput').val();
-    let photo     = form.find('#photoInput').val();
+    let firstName = form.find('firstName').val();
+    let lastName  = form.find('.lastNameInput').val();
+    let location  = form.find('.locationInput').val();
+    let phone     = form.find('.phoneInput').val();
+    let photo     = form.find('.photoInput').val();
 
     let newContact = new Contact (firstName, lastName, location, phone, photo);
 
-console.log(newContact);
+// console.log(newContact);
       // this.appendTemplate(contactForm);
       let input = this.contactForm.find('.input')
       input.val('');
   });
-
 
   // appendTemplate(contactObj) {
   //   `
@@ -46,50 +45,17 @@ console.log(newContact);
   //   `;
   // }
 }
+addContactToView(contactObj) {
+  let contactHTML = this.contactTemplate(contactObj)
+  this.contactList.append(contactHTML);
+};
 
+
+
+contactTemplate(contactObj) {
+  return`
+    <input id="${contactObj.fistName}">
+
+  `;
 }
-
-// import $ from 'jquery';
-//
-// import{ List } from '../models/list';
-// export class AppController {
-// //created contructor for the AppController class that has the properties form, listElem, and instance
-//   constructor(form, listElem, instance) {
-//     this.form = form;
-//     this.listArea = listElem;
-//     this.contactInstance = instance;
-//   }
-//
-//
-// //starts the application
-//   init() {
-//     //using an on click w submit property
-//     this.form.on('submit', (event) => {
-//       event.preventDefault();
-//       let input = this.form.find('input');
-//       this.addPersonToContacts(input.val());
-//       input.val('');
-//     });
-//     // console.log(this.addPersonToContacts);
-//   };
-//
-//   addPersonToContacts(listForm) {
-//     let l = new List(listForm);
-//     this.contactInstance.contactInfo.push(l);
-//     this.addListToView(l);
-//
-//   };
-//
-//   addListToView(listObj) {
-//     let listHTML = this.listTemplate(listObj);
-//     this.listArea.prepend(listHTML);
-//   };
-//
-// listTemplate(listObj) {
-//   return `<input class=$'{listObj.info.name}'>;
-//   <input class=$'{listObj.info.phone}'>;
-//   <input class=$'{listObj.info.location}'>;
-//   `
-// };
-//
-// };
+}

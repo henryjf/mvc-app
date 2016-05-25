ffe925600b5c0ebe70f2afdf4f4f9903c991ed25
JEFF HENRY <henry_jf@hotmail.com>
2016-05-25 14:37:51 -0400

    completed testing

--- test/test.js
+++ test/test.js
@@ -1,24 +1,53 @@
 // Import Chai
 import chai from 'chai';
+import $ from 'jquery';
 
 // Import Any Files to Test
-import '../src/js/main';
+// import '../src/js/main';
+import { Contact } from '../src/js/models/contact';
+import { AppController } from '../src/js/controllers/appController';
 
 // Set Chai Constants
 const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('Something We Want To Test', function () {
+describe('Testing our Contact Class', function () {
 
-  describe('Testing the Creation of Something', function () {
+  let contact;
 
-    it('should exist after we create it', function () {
-      let x = new Object;
-      expect(x).to.be.an.instanceof(Object);
+  beforeEach( function () {
+    contact= new Contact();
+  });
+
+  // describe('Testing the Creation of Something', function () {
+
+    it('should be an instance of contact', function () {
+      contact = new Contact;
+      expect(contact).to.be.an.instanceof(Contact);
     });
 
+  // });
+
+
+});
+
+describe('Testing our AppController Class', function () {
+
+  let appController;
+
+  beforeEach( function () {
+     appController= new AppController();
   });
+
+  // describe('Testing the Creation of Something', function () {
+
+    it('should be an instance of appController', function () {
+      appController = new AppController;
+      expect(appController).to.be.an.instanceof(AppController);
+    });
+
+  // });
 
 
 });
